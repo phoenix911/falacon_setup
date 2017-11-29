221839a578ea6ce8202dd8ae3bcbdee356c544a4
sangeet <sangeet@servx.com>
2017-11-29 06:21:47 +0530

    db added

--- model/__init__.py
+++ model/__init__.py
@@ -1,13 +1,2 @@
 # created by sangeet verma for project falcon_basics
 # on 27/11/17 :: 1:02 AM
-
-from mongoengine import register_connection
-
-alias = 'core'
-
-
-def global_connection():
-    register_connection(
-        alias=alias,
-        name='setup_demo'
-    )
